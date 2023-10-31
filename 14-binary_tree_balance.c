1791e6ff867837bc47f7508cd192607b06ce36d0
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-10-31 15:50:23 -0700

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,16 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int right = 0, left = 0, total = 0;
+
 	if (tree)
 	{
-		return (binary_tree_height(tree->left) -
-				binary_tree_height(tree->right));
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
+		total = left - right;
 	}
 
-	return (0);
+	return (total);
 }
 
 /**
@@ -25,13 +28,19 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t l = 0, r = 0;
+	size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		l = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+	if (!tree)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+		}
 		return ((l > r) ? l : r);
 	}
-	return (0);
 }
