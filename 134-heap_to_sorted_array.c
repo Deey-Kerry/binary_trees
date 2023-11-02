b1c4924ee1aeaba10131d7bf62901b4c78827235
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-02 06:48:29 -0700

    134-heap_to_sorted_array.c

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -6,16 +6,16 @@
  */
 size_t tree_size(const binary_tree_t *tree)
 {
-	size_t height_l = 0, height_r = 0;
+	size_t height_left = 0, height_right = 0;
 
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
-		height_l = 1 + tree_size(tree->left);
+		height_left = 1 + tree_size(tree->left);
 	if (tree->right)
-		height_r = 1 + tree_size(tree->right);
+		height_right = 1 + tree_size(tree->right);
 
-	return (height_l + height_r);
+	return (height_left + height_right);
 }
 
 /**
@@ -27,18 +27,18 @@ size_t tree_size(const binary_tree_t *tree)
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int k = 0, *a = NULL;
+	int k = 0, *b = NULL;
 
 	if (!heap || !size)
 		return (NULL);
 	*size = tree_size(heap) + 1;
-	a = malloc(sizeof(int) * (*size));
+	b = malloc(sizeof(int) * (*size));
 
-	if (a == NULL)
+	if (b == NULL)
 		return (NULL);
 
 	for (; heap; k++)
-		a[i] = heap_extract(&heap);
+		b[k] = heap_extract(&heap);
 
-	return (a);
+	return (b);
 }
